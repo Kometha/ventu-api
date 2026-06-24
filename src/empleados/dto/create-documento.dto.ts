f523ec5deac22a9c42146b5fcbbfe2d89d560ad5
Keneth <kenethcubas2002@unitec.edu>
2026-06-24 13:30:40 -0600

    create documento dto

---
+++ src/empleados/dto/create-documento.dto.ts
@@ -0,0 +1,4 @@
+import { CreateEmpleadoDocumentoDto } from './create-empleado.dto';
+
+// DTO usado por el endpoint POST /empleados/:id/documentos
+export class CreateDocumentoDto extends CreateEmpleadoDocumentoDto {}
